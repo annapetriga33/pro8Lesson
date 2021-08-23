6685876f98b2f71705d8cd833c5cb7f3d643800c
annapetriga33 <annapetriga33@gmail.com>
2021-08-23 17:50:55 +0300

    first commit

---
+++ custom.js
@@ -0,0 +1,32 @@
+var data1 = ['a', 'b', 'c'];
+data1.push(1, 2, 3);
+console.log(data1);
+
+///////////////////////
+
+numbers1 = [1, 2, 3];
+for (var i = numbers1.length - 1; i >= 0; i--) {
+    console.log(numbers1[i]);
+}
+
+///////////////////////////
+
+var data2 = [1, 2, 3];
+data2.unshift(4, 5, 6);
+console.log(data2);
+
+////////////////////////
+
+
+var users = [1, 2, 3, 4, 5];
+var usersNow = users.slice(0, 3);
+console.log(usersNow);   
+
+/////////////////////
+
+var data3 = ['js', 'css', 'jq'];
+var data4 = data3[0];
+console.log(data4);
+
+///////////////////////
+
